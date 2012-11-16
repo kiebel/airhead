ed4c1dbe420064ffde63f14beff846ea767789c8
Thomas Kiebel <tomuchan76@gmail.com>
2012-11-16 11:34:33 +0100

    adds initial launcher and preliminary setup

---
+++ airhead.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Airhead - Python based ActiveSync Server
+
+Launch server according to configuration and command-line options.
+"""
+import airhead.__main__
+
+airhead.__main__.run()
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#
+"""
+Airhead - Python based ActiveSync Server
+========================================
+
+The Airhead project implements an ActiveSync server. It is intended to be a
+light solution that is easy to install, configure and use. The objective is to
+provide an ActiveSync server that can be used as either standalone server or a
+module for other projects (e.g. Radicale see http://www.radicale.org).
+
+Airhead runs on most of the UNIX-like platforms (Linux, BSD, MacOS X) and
+Windows. It is free and open-source software, released under GPL version 3.
+"""
